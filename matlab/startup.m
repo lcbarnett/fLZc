c426df612f8e7a5c3b20523081637bcf53914fd9
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2020-10-30 17:34:59 +0000

    Amended startup.m

--- matlab/startup.m
+++ matlab/startup.m
@@ -4,14 +4,14 @@ global LZc_version;
 LZc_version.major = 1;
 LZc_version.minor = 0;
 
-fprintf('[LZc startup] Initialising LZc version %d.%d\n', LZc_version.major, LZc_version.minor);
+fprintf('[fLZc startup] Initialising LZc version %d.%d\n', LZc_version.major, LZc_version.minor);
 
 % Add LZc root dir + appropriate subdirs to path
 
 global LZc_root;
 LZc_root = fileparts(mfilename('fullpath')); % directory containing this file
 addpath(genpath(LZc_root));
-fprintf('[LZc startup] Added path %s\n',LZc_root);
+fprintf('[fLZc startup] Added path %s\n',LZc_root);
 
 % Binary data files: download zip file
 %
@@ -25,4 +25,4 @@ fLZc_data_path = getenv('FLZC_DATA_PATH');
 assert(~isempty(fLZc_data_path), 'You must define the environmental variable ''FLZC_DATA_PATH''');
 assert(exist(fLZc_data_path,'dir') == 7,'bad data path: ''%s'' does not exist or is not a directory',fLZc_data_path);
 
-fprintf('[LZc startup] Initialised (you may re-run `startup'' at any time)\n');
+fprintf('[fLZc startup] Initialised (you may re-run `startup'' at any time)\n');
