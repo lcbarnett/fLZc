1d2af7896009ebf46bac7e378aeef6ef82a3a0da
lcbarnett <68392448+lcbarnett@users.noreply.github.com>
2021-06-18 11:06:42 +0100

    Update LZ76c.m
    Remove misleading commented-out code if not using mex

--- matlab/LZ76c.m
+++ matlab/LZ76c.m
@@ -24,20 +24,7 @@ if use_mex
 	c = LZ76c_mex(s);
 
 else
+
 	error('sorry, not implemented yet');
-%{
-	dict = containers.Map;      % the dictionary
-	w = [];                     % initialise current word
-	for ch = s                  % iterate through input characters
-		w = [w ch];             % append current character to current word
-		if ~isKey(dict,w)       % if current word not already in dictionary...
-			dict(w) = true;     % add to dictionary
-			w = [];             % and reinitialise
-		end
-	end
-	c = length(dict);           % LZ complexity is size of dictionary
-	if nargout > 1
-		dict = keys(dict)';     % cell string of dictionary words
-	end
-%}
+
 end
