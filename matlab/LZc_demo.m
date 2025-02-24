6b31f61a1709f81d19528db6633e4297485f05f3
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2025-02-24 10:43:07 +0000

    Deprecated LZ76 demo (now incorporated in LZ demo)

--- matlab/LZc_demo.m
+++ matlab/LZc_demo.m
@@ -1,18 +1,19 @@
 % LZc demo script: generate an (approximate) Ornstein Uhlenbeck time series, and calculate
 % normalised LZ-complexity at all sequence lengths for a range of quantisation levels.
 %
-% Supply:
-%
-% maxn     % length of time series (max 100000)
-% maxq     % maximum quantisation level
-% fs       % sampling frequency (Hz)
-% oudec    % OU process decay parameter (> 0); smaller oudec gives smoother process
-%
-% e.g. (cut & paste)
-%{
-maxn = 100000; maxq = 5; fs = 200; oudec = 1; LZc_demo
-%}
+% Default parameters (may be overriden on command line)
 
+defvar('maxn',    100000    ); % length of time series (max 100,000)
+defvar('maxq',    5         ); % maximum quantisation
+defvar('fs',      200       ); % sampling frequency (Hz)
+defvar('oudec',   1         ); % OU process decay parameter (> 0); smaller oudec gives "smoother" process
+defvar('algo',   'LZ'       ); % Lempel-Ziv algorithm: 'LZ' or 'LZ76'
+
+switch upper(algo)
+	case 'LZ',   lz76 = false; algostr = 'LZc';
+	case 'LZ76', lz76 = true;  algostr = 'LZ76c';
+	otherwise, error('Lempel-Ziv algorithm must be ''LZ'' or ''LZ76''');
+end
 % Generate an (approximate) Ornstein-Uhlenbeck time series
 
 fprintf('\ngenerating OU time series... ');
@@ -29,15 +30,20 @@ fprintf('done\n\n');
 
 % Calculate complexities for different quantisation levels, and load random string complexities
 
-c      = zeros(maxn,maxq);             % complexities of x
-crnd   = zeros(maxn,maxq);             % random string complexities
-qtiles = cell(maxq,1);                 % quantiles
-for q = 1:maxq                         % for each quantisation
+c      = zeros(maxn,maxq);                 % complexities of x
+crnd   = zeros(maxn,maxq);                 % random string complexities
+qtiles = cell(maxq,1);                     % quantiles
+for q = 1:maxq                             % for each quantisation
 	fprintf('processing qauntisation %d of %d... ',q,maxq);
-	d = q+1;                           % alphabet size = number of quantiles + 1
-	[s,qtiles{q}] = LZc_quantise(x,q); % quantise noise sequence by q quantiles; store quantiles
-	c(:,q) = LZc_x(s,d);               % calculate "running" LZ complexity (i.e., for all sequence lengths to maximum)
-	crnd(:,q) = LZc_crand(1:maxn,d);   % load random string mean complexities from file
+	d = q+1;                               % alphabet size = number of quantiles + 1
+	[s,qtiles{q}] = LZc_quantise(x,q);     % quantise noise sequence by q quantiles; store quantiles
+	if lz76
+		c(:,q) = LZ76c_x(s,d);             % calculate "running" LZ76 complexity (i.e., for all sequence lengths to maximum)
+		crnd(:,q) = LZ76c_crand(1:maxn,d); % load random string mean complexities from file
+	else
+		c(:,q) = LZc_x(s,d);               % calculate "running" LZ complexity (i.e., for all sequence lengths to maximum)
+		crnd(:,q) = LZc_crand(1:maxn,d);   % load random string mean complexities from file
+	end
 	fprintf('done\n');
 end
 cnorm = c./crnd;                       % complexities normalised by mean complexity of random strings of same length and alphabet size
@@ -63,7 +69,7 @@ for q = 1:maxq
 end
 % sgtitle('Time series with quantisations'); % only version >= 2018b
 
-% Estimate power spectral density of time series
+% Estimate power spectral density (PSD) of time series
 
 [S,f] = pwelch(x,[],[],[],fs);         % estimated PSD (Welch method)
 w = 2*pi*f/fs;                         % normalised frequency in [0,pi]
@@ -86,9 +92,9 @@ subplot(2,1,2);
 semilogx(n,cnorm);
 ylim([0 1.2]);
 yline(1,'color','k');
-title('normalised LZ-complexity');
+title(sprintf('normalised %s complexity',algostr));
 xlabel('sequence length (log-scale)');
-ylabel('LZc');
+ylabel('complexity');
 leg = legend(num2str((1:maxq)','%2d'),'location','southwest');
 leg.Title.Visible = 'on';
 title(leg,'quantiles');
